7dfc7f02cf802d30e6a1b2add67e3872175bc0be
Kristian Ebdrup <kristian.ebdrup@alexandra.dk>
2025-06-08 09:18:58 +0200

    WIP

--- setup_in_container.sh
+++ setup_in_container.sh
@@ -1,3 +1,3 @@
 echo "Setting up neovim in $1"
 
-docker exec $1 /bin/bash -c "apt update && apt install git -y; (cd ~; git clone https://github.com/Kebdrup/neovim_dev.git; ./neovim_dev/config_install.sh)"
+docker exec $1 /bin/bash -c "apt update && apt install git -y; (cd ~; rm -rf ./neovim_dev; git clone https://github.com/Kebdrup/neovim_dev.git; ./neovim_dev/config_install.sh)"
