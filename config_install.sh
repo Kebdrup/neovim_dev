32c2cf97b007b97c17d9a21c61ee18837ff7d4bf
Kristian Ebdrup <kristian.ebdrup@alexandra.dk>
2025-07-14 16:27:31 +0200

    Update config with '-r' options

--- config_install.sh
+++ config_install.sh
@@ -2,34 +2,62 @@
 
 install_dir="/nvim"
 
-apt update 
+setup_container() {
+  # Update and install dependencies
+  apt update 
+  apt install tmux git ripgrep
 
-apt install tmux git ripgrep
+  # If neovim is not installed then install it
+  if ! command -v nvim >/dev/null 2>&1
+  then
+    mkdir -p $install_dir
+    wget https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-arm64.appimage -O $install_dir/nvim.appimage
+    chmod u+x $install_dir/nvim.appimage
+    (cd $install_dir; ./nvim.appimage --appimage-extract)
+    ln -s $install_dir/squashfs-root/usr/bin/nvim /usr/bin/nvim && ln -s $install_dir/squashfs-root/usr/bin/nvim /usr/bin/vim
+  fi
 
-# If neovim is not installed then install it
-if ! command -v nvim >/dev/null 2>&1
-then
-	mkdir -p $install_dir
-	wget https://github.com/neovim/neovim/releases/download/v0.11.1/nvim-linux-arm64.appimage -O $install_dir/nvim.appimage
-	chmod u+x $install_dir/nvim.appimage
-	(cd $install_dir; ./nvim.appimage --appimage-extract)
-	ln -s $install_dir/squashfs-root/usr/bin/nvim /usr/bin/nvim && ln -s $install_dir/squashfs-root/usr/bin/nvim /usr/bin/vim
-fi
+  # Depedencies for treesitter
+  apt install -y npm build-essential
+  # apt install -y nodejs # TODO: nodejs should probably be installed another way
+  echo "Installing tree-sitter-cli"
+  npm install -g tree-sitter-cli
+
+  # Add git config
+  cp -v -a -r $HOME/neovim_dev/git/. $HOME/
+
+  # Add bash configs
+  $HOME/neovim_dev/bash/extend_bashrc.sh
+
+}
+
+remote=false
+
+# Parse options
+while getopts 'r' opt; do
+  case "$opt" in
+    r) setup_container; remote=true;;
+  esac
+done
+shift $((OPTIND -1))
+
 
 # Create config directory
 config_dir="$HOME/.config"
+
+if [ "$1" == "" ] || [ $# -gt 1 ]; then
+  echo "No config directory provided, using: $config_dir"
+else
+  config_dir="$1"
+  echo "Using config directory: $config_dir"
+fi
+
 mkdir -p $config_dir
 
-# Depedencies for treesitter
-apt install -y npm build-essential
-# apt install -y nodejs # TODO: nodejs should probably be installed another way
-echo "Installing tree-sitter-cli"
-npm install -g tree-sitter-cli
-
 # Copy configs
-cp -v -R $HOME/neovim_dev/config/* $HOME/.config/ 
+if $remote; then
+  cp -v -R $HOME/neovim_dev/config/* $config_dir 
+else
+  cp -v -R ./config/* $config_dir 
+fi
 
-cp -v -a -r $HOME/neovim_dev/git/. $HOME/
-
-# Add bash configs
-$HOME/neovim_dev/bash/extend_bashrc.sh
